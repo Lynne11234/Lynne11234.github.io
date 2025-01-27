395bac19a1d2bc04f0dd86714a3d58e717e79023
Lynne11234 <nlynne@ocadu.ca>
2025-01-27 09:00:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+0; //this is a comment, which means it will not be executed
+12345678;
+Infinity; 
+
+var jimmy; // declared a variable for store space in memory;
+jimmy = 5; // defining a variable jimmy and assigning it a value of 5;
+var Jimmy= 20; //semi are end of the statement, this line has a declaration and definition of a variable Jimmy;
+console.log(jimmy); //output the value of jimmy to the console;
+jimmy=Jimmy; //assigning the value of Jimmy to jimmy;
+console.log(jimmy); //output the value of jimmy to the console;
+var joe;
+console.log(joe); //output the value of joe to the console;
+var prose= 'ghsgygdygdsj'; // this is a  string data type;
+var moreprose= "dhudhewifhikj"; // this is a string data type;
+var x= true;
+x= false;
+
+// objects
+var ball={}; // emmpty object
+ball= null; // null is a datatype in javascript that means nothing;
+// operators
+5 + 5; // addition operator
+jimmy= 5+5;
+var jimmy; //becomes undefined
+
+jimmy= 5+"5";
+console.log(jimmy);
